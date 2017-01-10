55cb2c814c35d9598725a6080ff03345dfd9f534
Stephan Kranenfeld <stephankranenfeld@hotmail.com>
2017-01-10 10:52:24 +1000

    swapped from multi threading to asyncrones setup.

--- SocketServer/ServerStart.cs
+++ SocketServer/ServerStart.cs
@@ -5,14 +5,18 @@ using System.Net;
 using System.Net.Sockets;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 using System.Threading;
 
 //stuff
 
 namespace SocketServer
 {
+
     class ServerStart
     {
+        // Thread signal.
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
         //list of our clients. singleton
         private static ClientList _clientList = ClientList.Instance;
         private static TcpListener _listener = null;
@@ -28,7 +32,7 @@ namespace SocketServer
             Console.ReadLine();
         }
 
-        private async void SetupServer()
+        private void SetupServer()
         {
             Console.WriteLine("Setting up server...");
             _listener = new TcpListener(IPAddress.Any, 3000);
@@ -37,49 +41,57 @@ namespace SocketServer
             bool listening = true;
             while (listening)
             {
-                //non blocking await, so can accept multiple clients
-                TcpClient client = await _listener.AcceptTcpClientAsync().ConfigureAwait(false);
-                Client c = new Client(client);
-                _clientList.Add(c);
-                Console.WriteLine("client connected");
-                //handle client in new thread
-                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
-                t.Start(c);
+                // Set the event to nonsignaled state.
+                allDone.Reset();
+
+                // Start an asynchronous socket to listen for connections.
+                //Console.WriteLine("Waiting for a connection...");
+                _listener.BeginAcceptTcpClient(HandleClient, _listener);
+
+                // Wait until a connection is made before continuing.
+                allDone.WaitOne();
             }
         }
 
         //new parameterizedthreadstart only allows obj
-        public static void HandleClient(object obj)
+        public static void HandleClient(IAsyncResult ar)
         {
+            // Get the listener that handles the client request.
+            _listener = (TcpListener)ar.AsyncState;
+            Client client = new Client(_listener.EndAcceptTcpClient(ar));
+            _clientList.Add(client);
+
+            // Signal the main thread to continue.
+            allDone.Set();
+
             //buffer, change size later if needed.
             byte[] buffer = new byte[1024];
 
-            //cast obj to a tcp client
-            Client client = (Client)obj;
-            
             // Get a stream object for reading and writing
             NetworkStream stream = client.tcp.GetStream();
             while (client.tcp.Connected)
             {
-                try {
+                try
+                {
                     IMessage msg;
 
                     IFormatter formatter = new BinaryFormatter();
 
                     msg = (IMessage)formatter.Deserialize(stream);
-                    
+
                     //should adjust this to be switch with types.
-                    if(msg is Player)
+                    if (msg is Player)
                     {
                         Player player = (Player)msg;
-                    }else if(msg is Message)
+                    }
+                    else if (msg is Message)
                     {
                         Message message = (Message)msg;
                         Console.WriteLine("Received: {0}", message.message);
                     }
                     OutBound.SendTextMessage(msg);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     //Console.WriteLine("error: " + e);
                 }
