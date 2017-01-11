591e5ba3df491d3c0d5e07c611c97e394e9bda1d
Stephan Kranenfeld <stephankranenfeld@hotmail.com>
2017-01-11 23:27:45 +1000

    stufff

--- SocketClient/Client.cs
+++ SocketClient/Client.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -52,15 +53,42 @@ namespace SocketClient
 
         private static void SendMessage(NetworkStream stream, TcpClient client)
         {
-            IFormatter formatter = new BinaryFormatter();
             while (client.Connected)
             {
                 string input = Console.ReadLine();
-                var msg = new Message(input, MessageType.Async);
-                //write to out stream.
-                var msg2 = (IMessage)msg;
-                formatter.Serialize(stream, msg2);
+                Message msg = new Message(input, MessageType.Async);
+                byte[] buffer = new byte[1024];
+                byte[] bMsg = MyByteConverter.ObjectToByteArray(msg);
+                //Console.WriteLine("converted");
+                int size = bMsg.Length;
+                byte[] lengthBytes = BitConverter.GetBytes(size);
+                Buffer.BlockCopy(lengthBytes, 0, buffer, 0, lengthBytes.Length);
+                Buffer.BlockCopy(bMsg, 0, buffer, lengthBytes.Length - 1, bMsg.Length);
+                stream.Write(buffer, 0, buffer.Length);
             }
+        }
+
+        public static void EndRead(IAsyncResult ar)
+        {
+            BuffStream bs = (BuffStream)ar.AsyncState;
+            bs.stream.EndRead(ar);
+            byte[] buffer = bs.buffer;
+            byte[] msgLengthBytes = new byte[sizeof(int)];
+
+            //grab the size of the payload.
+            Buffer.BlockCopy(buffer, 0, msgLengthBytes, 0, msgLengthBytes.Length);
+            int size = BitConverter.ToInt32(msgLengthBytes, 0);
+            byte[] payload = new byte[size];
+
+            //grab the payload using the size
+            Buffer.BlockCopy(buffer, sizeof(int) - 1, payload, 0, size);
+            //convert it to an actual message object
+            Message msg = (Message)MyByteConverter.ByteArrayToObject(payload);
+
+            //OutBound.SendTextMessageOff(msg);
+            //Console.WriteLine("test");
+
+            Console.WriteLine("Received: {0}", msg.message);
         }
 
         private static void IncomingMessages(object obj)
@@ -71,23 +99,27 @@ namespace SocketClient
             //buffer, change size later if needed.
             byte[] buffer = new byte[1024];
 
+            int errorCount = 0;
+
             while (true)
             {
                 try
                 {
-                    //new message object
-                    Message msg = new Message(null, MessageType.Async);
-                    IFormatter formatter = new BinaryFormatter();
-
-                    //recive message
-                    msg = (Message)formatter.Deserialize(stream);
-
-                    Console.WriteLine("Received: {0}", msg.message);
-                    //BroadcastMessage(clientMessage);
+                    stream.BeginRead(buffer, 0, buffer.Length, EndRead, new BuffStream(stream, buffer));
+                    buffer = new byte[1024];
+                    errorCount = 0;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("error: " + e);
+                    if (errorCount == 0)
+                        Console.WriteLine("error: " + e);
+
+                    errorCount++;
+                    if (errorCount > 10)
+                    {
+                        Console.WriteLine("Disconnected from server");
+                        break;
+                    }
                 }
             }
         }
